46c17e2c28907b526317b49cb3cc58552b1e0f4b
ANTCS <antonio.cassano@nhsbsa.nhs.uk>
2025-04-22 11:09:58 +0100

    Initial commit

---
+++ src/main/java/com/nhsbsa/band6/antcs/Band6DeveloperAssignmentAntonioCassanoApplication.java
@@ -0,0 +1,13 @@
+package com.nhsbsa.band6.antcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Band6DeveloperAssignmentAntonioCassanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Band6DeveloperAssignmentAntonioCassanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhsbsa/band6/antcs/Band6DeveloperAssignmentAntonioCassanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhsbsa.band6.antcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Band6DeveloperAssignmentAntonioCassanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
